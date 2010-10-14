3f2ed6f14e9e8c344e30f3691c0ea2b87efdd08a
Hay <huskyr@gmail.com>
2010-10-15 01:03:22 +0200

    added branch for live site

--- index.php
+++ index.php
@@ -69,5 +69,26 @@ if (isset($_POST['submit'])) {
         <input type="submit" name="submit" value="GO" />
     </form>
 </div> <!-- /wrapper -->
+
+<!-- Clicky -->
+<script src="http://static.getclicky.com/15420.js" type="text/javascript"></script>
+<noscript><p><img alt="Clicky" src="http://in.getclicky.com/15420ns.gif" /></p></noscript>
+
+<!-- /Clicky -->
+
+<!-- Google Analytics -->
+<script> 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-8052007-2']);
+  _gaq.push(['_trackPageview']);
+ 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script> 
+<!-- /Google Analytics -->
+
 </body>
 </html>
